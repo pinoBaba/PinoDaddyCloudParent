b0b02f358a0c23ff1a6d7062f08c30725d71915c
pinoBaba <2656350287@qq.com>
2021-10-24 19:47:23 +0800

    parent 父级模块pom添加consul client依赖，修改了启动consul server的批处理. 新建了子模块consul client

---
+++ p-d-spring-cloud-6-consul-client-demo/src/main/java/org/pd/consul/client/ConsulClientApplication.java
@@ -0,0 +1,13 @@
+package org.pd.consul.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClientApplication.class,args);
+    }
+}
--- start-consul-server.bat
+++ start-consul-server.bat
@@ -1,5 +1,6 @@
 @echo off
 %//0黑 1蓝 2绿 3浅绿..%
 color 06
-cd your consul server path    %D:\consul_1.10.3_windows_amd64%
+d:
+cd D:\consul_1.10.3_windows_amd64     %D:\consul_1.10.3_windows_amd64%
 consul agent -dev
