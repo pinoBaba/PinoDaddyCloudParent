e543373e6ff49bf9349649587ccde9a56ef4dbfb
pinoBaba <2656350287@qq.com>
2021-10-26 21:56:12 +0800

    修复了启动consul server批处理文件的bug

--- start-consul-server.bat
+++ start-consul-server.bat
@@ -1,6 +1,5 @@
 @echo off
 %//0黑 1蓝 2绿 3浅绿..%
 color 06
-d:
-cd D:\consul_1.10.3_windows_amd64     %D:\consul_1.10.3_windows_amd64%
-consul agent -dev
+%your consul server install path %
+cmd /k "cd /d D:\consul_1.10.3_windows_amd64&&consul agent -dev"
