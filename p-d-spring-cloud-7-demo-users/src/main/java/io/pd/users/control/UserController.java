5a15dc3f10d259d2fbbe5708829ea9b5019f794e
pinoBaba <2656350287@qq.com>
2021-10-28 21:33:13 +0800

    注入RestTemplate并添加@LoadBalanced注解 使其具备负载均衡能力并被spring工厂管理 user中添加方法  以rest直接调用order时 修改类实例以构造器注入

--- p-d-spring-cloud-7-demo-users/src/main/java/io/pd/users/control/UserController.java
+++ p-d-spring-cloud-7-demo-users/src/main/java/io/pd/users/control/UserController.java
@@ -1,22 +1,24 @@
 package io.pd.users.control;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-/* spring提供的发起rest请求的能力，与SpringCloud无关 */
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/* spring提供的发起rest请求的能力，与SpringCloud无关 */
+
 @RestController
 @RequestMapping("users")
 public class UserController {
+
+    private final String INVOKE_ORDER_METHOD = "/orders";
 
     @GetMapping
     public String rest(){
@@ -29,7 +31,7 @@ public class UserController {
         System.out.println("Start to invoke order ");
 
         RestTemplate restTemplate = new RestTemplate();
-        final String result = restTemplate.getForObject(chooseNode()+"/orders", String.class);
+        final String result = restTemplate.getForObject(chooseNode()+INVOKE_ORDER_METHOD, String.class);
         System.out.println("end to invoke order ,please see result: "+result);
 
         return "users call success";
@@ -40,7 +42,7 @@ public class UserController {
      */
     private String chooseNode(){
         //模拟存储节点信息
-        List<String> nodeAddress = new ArrayList<String>();
+        List<String> nodeAddress = new ArrayList<>();
         //添加节点
         nodeAddress.add("http://localhost:8006");
         nodeAddress.add("http://localhost:8007");
@@ -55,10 +57,10 @@ public class UserController {
     /**
      * 以SpringCloud提供得组件来发其rest请求
      * 1 DiscoverClient 2LoadBalanceClient  3LoadBalance
-     * @return
+     *
+     * @return order服务的相应信息
      */
     // 1
-    @Autowired
     private DiscoveryClient discoveryClient;
 
     @GetMapping("call/discoveryClient/orders")
@@ -70,13 +72,12 @@ public class UserController {
 
         System.out.println( serviceInstance.getHost() + serviceInstance.getPort()   );
 
-        final String res = new RestTemplate().getForObject(serviceInstance.getUri() + "/orders", String.class);
+        final String res = new RestTemplate().getForObject(serviceInstance.getUri() + INVOKE_ORDER_METHOD, String.class);
         System.out.println(res);
         return "users call success";
     }
 
     // 2
-    @Autowired
     private LoadBalancerClient loadBalancerClient;
     @GetMapping("call/LoadBalancerClient/orders")
     public String invokeOrdersService3(){
@@ -86,10 +87,28 @@ public class UserController {
 
         System.out.println( orders.getHost() + orders.getPort()   );
 
-        final String res = new RestTemplate().getForObject(orders.getUri() + "/orders", String.class);
+        final String res = new RestTemplate().getForObject(orders.getUri() + INVOKE_ORDER_METHOD, String.class);
         System.out.println();
         return res;
     }
 
+    //http;//ORDERS需要被ribbon客户端解析才能返回具体节点地址，直接访问找不到
+    private final String ORDER_SERVICE_ADDRESS = "http://ORDERS";
+
+    //以构造器注入工程管理的bean进来   (需要考虑 构造器注入的属性过多 当前类是否违背了单一职责)
+    private UserController( RestTemplate restTemplate , LoadBalancerClient loadBalancerClient , DiscoveryClient discoveryClient ){
+        this.restTemplate = restTemplate;
+        this.loadBalancerClient = loadBalancerClient;
+        this.discoveryClient = discoveryClient;
+    }
+    private RestTemplate restTemplate;
+
+    //3 以ribbon组件+RestTemplate做服务通讯
+    @GetMapping("call/LoadBalanced/orders")
+    public String invokeOrderService4(){
+
+        return restTemplate.getForObject( ORDER_SERVICE_ADDRESS+INVOKE_ORDER_METHOD , String.class );
+
+    }
 
 }
